b4db9ab9d3ff7020528962a54ba63651018d88eb
João Adão <2200658@my.ipleiria.pt>
2021-04-30 13:02:14 +0100

    Histórico temperatura

--- historico.php
+++ historico.php
@@ -3,7 +3,77 @@
     $activePage="historico";
 ?>
 <?php include "header.php"; ?>
-
-
+<div class="row">
+<div class="col-12">
+      <div class="card-stats card">
+      <div class="card-header">
+                <h4 class="card-title">Temperatura</h4> 
+                <p class="card-category">Histórico do Sensor de Temperatura</p>
+            </div>
+         <div class="card-body">
+            <div class="table-responsive">
+                <table class="table table-hover table-striped">
+                    <thead>
+                        <th>Data</th>
+                        <th>Hora</th>
+                        <th>Valor Registado</th>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>2021-04-30</td>
+                            <td>17:30:00</td>
+                            <td>25 Cº</td>
+                        </tr>
+                        <tr>
+                            <td>2021-04-30</td>
+                            <td>17:30:00</td>
+                            <td>25 Cº</td>
+                        </tr>
+                        <tr>
+                            <td>2021-04-30</td>
+                            <td>17:30:00</td>
+                            <td>25 Cº</td>
+                        </tr>
+                        <tr>
+                            <td>2021-04-30</td>
+                            <td>17:30:00</td>
+                            <td>25 Cº</td>
+                        </tr>
+                        <tr>
+                            <td>2021-04-30</td>
+                            <td>17:30:00</td>
+                            <td>25 Cº</td>
+                        </tr>
+                        <tr>
+                            <td>2021-04-30</td>
+                            <td>17:30:00</td>
+                            <td>25 Cº</td>
+                        </tr>
+                        <tr>
+                            <td>2021-04-30</td>
+                            <td>17:30:00</td>
+                            <td>25 Cº</td>
+                        </tr>
+                        <tr>
+                            <td>2021-04-30</td>
+                            <td>17:30:00</td>
+                            <td>25 Cº</td>
+                        </tr>
+                        <tr>
+                            <td>2021-04-30</td>
+                            <td>17:30:00</td>
+                            <td>25 Cº</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+         </div>
+         <div class="card-footer">
+         <hr>
+                <div class="stats"><i class="fas fa-redo mr-1"></i> 2021/04/28 15:00:00</div>
+         </div>
+      </div>
+   </div>
+</div>
 
 <?php include "footer.php"; ?>
